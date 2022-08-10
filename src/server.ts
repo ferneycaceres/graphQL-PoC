dc3f13d5070ad02d7bfc53f09b566d68aa7b5fc3
ferneycaceres <ferneycaceres@gmail.com>
2022-08-10 18:20:56 +0200

    first commit

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// The GraphQL schema
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const PORT = 5300;
+
+server.listen(
+    { port : PORT},
+    () => console.log(`Hola Mundo API GraphQL http://localhost:${PORT}`)
+);
